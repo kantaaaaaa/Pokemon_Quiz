749e52c99080f65e545b5e141b51ba0c2551ddfd
kantaaaaaa <111953276+kantaaaaaa@users.noreply.github.com>
2024-07-08 08:31:25 +0900

    お気に入りのクイズだけが表示されるページを作った

--- pokemon_quiz/lib/favorite_quiz.dart
+++ pokemon_quiz/lib/favorite_quiz.dart
@@ -26,6 +26,7 @@ class _Favorite_quiz_Page extends State<Favorite_quiz_Page> {
   _Favorite_quiz_Page({required this.uid});
   List<Quiz_data> quiz_data = [];
   List<Favorite_data> favorite_data = [];
+  List<Quiz_data> favorite_quizdata = [];
 
   final quizdb_index = 0;
   String uid;
@@ -41,132 +42,129 @@ class _Favorite_quiz_Page extends State<Favorite_quiz_Page> {
   void _viewfirebase() async {
     final db = FirebaseFirestore.instance;
 
-    final favorite_event =
-        await db.collection('Favoritedata').where('uid', isEqualTo: uid).get();
+    final favorite_event = await db
+        .collection('Favoritedata')
+        //.orderBy('quiz_id', descending: false)
+        .where('uid', isEqualTo: uid)
+        .get();
     final favorite_docs = favorite_event.docs;
     final favorite_data =
         favorite_docs.map((doc) => Favorite_data.fromFirestore(doc)).toList();
     this.favorite_data = favorite_data;
 
-    final quiz_ids = favorite_data.map((favData) => favData.quiz_id).toList();
+    final quiz_id = favorite_data.map((favData) => favData.quiz_id).toList();
 
-    final event = await db
-        .collection('Quizdata')
-        .orderBy('quiz_id', descending: false)
-        .where('quiz_id', isEqualTo: quiz_ids)
-        .get();
-    final docs = event.docs;
-    final quiz_data = docs.map((doc) => Quiz_data.fromFirestore(doc)).toList();
-    this.quiz_data = quiz_data;
+    quiz_id.sort();
+
+    for (var element in quiz_id) {
+      final event = await db
+          .collection('Quizdata')
+          //.orderBy('quiz_id', descending: false)
+          .where('quiz_id', isEqualTo: element)
+          .get();
+      final docs = event.docs;
+      final pokemon = docs.map((doc) => Quiz_data.fromFirestore(doc)).toList();
+      this.quiz_data += pokemon;
+
+      print(element);
+    }
+
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('お気に入りクイズ一覧'),
-      ),
-      resizeToAvoidBottomInset: false,
-      body: GridView.builder(
-        itemCount: quiz_data.length,
-        itemBuilder: (context, index) {
-          // favorite_view.add(0);
-          print(favorite_view[index]);
-          return Column(
-            children: [
-              SizedBox(
-                child: Card(
-                  elevation: 4,
-                  margin: const EdgeInsets.all(6),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  clipBehavior: Clip.antiAliasWithSaveLayer, // 画像を丸角にする
-                  child: Column(
-                    children: [
-                      Stack(
-                        children: [
-                          if (quiz_data[index].imgURL == "")
-                            Image.network(
-                              "images/path.png",
-                              width: 1000,
-                              height: 120,
-                            )
-                          else
-                            Image.network(
-                              quiz_data[index].imgURL,
-                              width: 1000,
-                              height: 120,
-                            ),
-                        ],
-                      ),
-                      // タイトル
-                      Container(
-                        width: double.infinity,
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 4, horizontal: 8),
-                        child: Text(
-                          "正解名   " + quiz_data[index].answer,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 22,
-                          ),
-                        ),
-                      ),
-                      // 説明文
-                      Container(
-                        child: Column(
+        appBar: AppBar(
+          title: const Text('お気に入り一覧'),
+        ),
+        resizeToAvoidBottomInset: false,
+        body: GridView.builder(
+          itemCount: quiz_data.length,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                SizedBox(
+                  child: Card(
+                    elevation: 4,
+                    margin: const EdgeInsets.all(6),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    clipBehavior: Clip.antiAliasWithSaveLayer, // 画像を丸角にする
+                    child: Column(
+                      children: [
+                        Stack(
                           children: [
-                            Text(
-                              "1." +
-                                  quiz_data[index].first +
-                                  "    2." +
-                                  quiz_data[index].second,
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 16,
+                            if (quiz_data[index].imgURL == "")
+                              Image.network(
+                                "images/path.png",
+                                width: 1000,
+                                height: 120,
+                              )
+                            else
+                              Image.network(
+                                quiz_data[index].imgURL,
+                                width: 1000,
+                                height: 120,
                               ),
-                            ),
-                            Text(
-                              "3." +
-                                  quiz_data[index].third +
-                                  "    4." +
-                                  quiz_data[index].fourth,
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 16,
-                              ),
-                            ),
                           ],
                         ),
-                      ),
-                    ],
+                        // タイトル
+                        Container(
+                          width: double.infinity,
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 4, horizontal: 8),
+                          child: Text(
+                            "正解名   " + quiz_data[index].answer,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 22,
+                            ),
+                          ),
+                        ),
+                        // 説明文
+                        Container(
+                          child: Column(
+                            children: [
+                              Text(
+                                "1." +
+                                    quiz_data[index].first +
+                                    "    2." +
+                                    quiz_data[index].second,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                "3." +
+                                    quiz_data[index].third +
+                                    "    4." +
+                                    quiz_data[index].fourth,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
+                  width: 350,
+                  height: 230,
                 ),
-                width: 350,
-                height: 230,
-              ),
-            ],
-          );
-        },
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          mainAxisSpacing: 0,
-          crossAxisSpacing: 0,
-          childAspectRatio: 1.5,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => Quiz_add()),
-          );
-        },
-        label: const Text('クイズ追加'),
-        icon: const Icon(Icons.thumb_up),
-        backgroundColor: Colors.pink,
-      ),
-    );
+              ],
+            );
+          },
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 0,
+            crossAxisSpacing: 0,
+            childAspectRatio: 1.5,
+          ),
+        ));
   }
 }
